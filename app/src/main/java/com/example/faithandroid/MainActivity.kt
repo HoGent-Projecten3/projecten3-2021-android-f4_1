611d32ccb42f67134cb9548f1423fa76178b2d07
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-10-17 12:09:36 +0200

    aavraag gesprek button aangemaakt, card aangepast voor img

--- app/src/main/java/com/example/faithandroid/MainActivity.kt
+++ app/src/main/java/com/example/faithandroid/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.bulletinboard)
+       // setContentView(R.layout.menu_homescherm)
+        setContentView(R.layout.bulletinboard) // to see of the screen is good
     }
 }
