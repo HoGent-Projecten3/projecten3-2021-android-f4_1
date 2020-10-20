f7157e61a8b8a405ef460daa9f4dc9c3f16478d8
Vromman <adam.vromman@student.hogent.be>
2020-10-20 21:51:29 +0200

    action balk opgelost

--- app/src/main/java/com/example/faithandroid/TestActivity.kt
+++ app/src/main/java/com/example/faithandroid/TestActivity.kt
@@ -11,11 +11,7 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test);
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(findViewById(R.id.layoutToolBar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
