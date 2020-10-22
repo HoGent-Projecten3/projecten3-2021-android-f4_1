314b65db1cf201b034d2924f9997564a2ba30968
Lukas <lukas.kindt@student.hogent.be>
2020-10-22 19:58:22 +0200

    verbinding internet toegevoegd - niet afgewerkt

---
+++ app/src/main/java/com/example/faithandroid/OverviewViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.faithandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import androidx.databinding.DataBindingUtil
+import com.example.faithandroid.databinding.FragmentBulletinboardBinding
+
+
+
+
+class BulletinboardFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+      val binding = DataBindingUtil.inflate<FragmentBulletinboardBinding>(inflater, R.layout.fragment_bulletinboard, container, false);
+        return binding.root
+    }
+}
