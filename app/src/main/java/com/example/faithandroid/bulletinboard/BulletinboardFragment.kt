0ec8896aa29fc9ec7b2b502fc42c4fcaf6feaf38
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-11-09 14:25:27 +0100

    bulletinboard mock posts aanpassen

--- app/src/main/java/com/example/faithandroid/bulletinboard/BulletinboardFragment.kt
+++ app/src/main/java/com/example/faithandroid/bulletinboard/BulletinboardFragment.kt
@@ -24,8 +24,6 @@ class BulletinboardFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-
     }
 
     override fun onCreateView(
