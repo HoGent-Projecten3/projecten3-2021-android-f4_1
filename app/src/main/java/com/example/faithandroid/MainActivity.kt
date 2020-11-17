090400a64611e1453cf9738a67fa7c3e4e751beb
’Pieter <azize.ergun@student.hogent.be>
2020-11-17 13:02:47 +0100

    begonnen aan het scherm

--- app/src/main/java/com/example/faithandroid/MainActivity.kt
+++ app/src/main/java/com/example/faithandroid/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.faithandroid
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -50,6 +53,32 @@ class MainActivity : AppCompatActivity(),DrawerInterface,NavigationView.OnNaviga
         supportActionBar?.setHomeButtonEnabled(true)
         actionBar?.setDisplayHomeAsUpEnabled(true)
         actionBar?.setHomeButtonEnabled(true)
+
+        //spinner (dropdownlist plaatsen)
+        // access the items of the list
+        val plaatsen = resources.getStringArray(R.array.Plaatsen)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, plaatsen)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@MainActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + plaatsen[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 
     override fun onBackPressed() {
